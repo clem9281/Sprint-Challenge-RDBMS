a2b356c7dadeb602bdbd38644bbe9aa1312a2ff1
clem9281 <clem9281@gmail.com>
2019-03-29 09:19:44 -0700

    added a post project endpoint

--- data/dbHelpers.js
+++ data/dbHelpers.js
@@ -4,6 +4,16 @@ const findProjects = () => {
   return db("projects");
 };
 
+const findProjectById = id => {
+  return db("projects").where({ id });
+};
+
+const addProject = newProject => {
+  return db("projects").insert(newProject);
+};
+
 module.exports = {
-  findProjects
+  findProjects,
+  addProject,
+  findProjectById
 };
--- routers/projects.js
+++ routers/projects.js
@@ -2,15 +2,47 @@ const express = require("express");
 const db = require("../data/dbHelpers");
 const router = express.Router();
 
-router.route("/").get(async (req, res) => {
-  try {
-    const projects = await db.findProjects();
-    res.status(200).json(projects);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Sorry we couldn't get the projects at this time" });
-  }
-});
+const prettifyArray = objArr => {
+  return objArr.map(el => ({
+    ...el,
+    completed: el.completed === 1 ? true : false
+  }));
+};
+
+const prettifyObject = obj => {
+  return { ...obj, completed: obj.completed === 1 ? true : false };
+};
+
+router
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      const projects = await db.findProjects();
+      // show true/false rather than 0 1
+      const prettyProjects = prettifyArray(projects);
+      res.status(200).json(prettyProjects);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Sorry we couldn't get the projects at this time" });
+    }
+  })
+  .post(async (req, res) => {
+    const { name, description } = req.body;
+    if (!name || !description)
+      return res
+        .status(400)
+        .json({ error: "Your new project must have a name and description" });
+    try {
+      const newProjectId = await db.addProject(req.body);
+      const newProject = await db.findProjectById(newProjectId[0]).first();
+      const prettierProject = prettifyObject(newProject);
+      res.status(201).json(prettierProject);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Sorry we couldn't add a project at this time" });
+    }
+  });
 
 module.exports = router;
